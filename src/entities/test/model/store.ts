5bc3c0cb7ea7699b81bb1d127f9a4dc71d75b025
KrasnoseBeloe2211 <kirillburlaka.2006@gmail.com>
2026-03-21 12:30:50 +0300

    query fix

--- src/entities/test/model/store.ts
+++ src/entities/test/model/store.ts
@@ -1,35 +1,38 @@
-import { Test } from "./types"
+import { Test } from './types'
 import { create } from 'zustand'
 
 interface ITestsStore {
-    tests: Test[];
-    loading: boolean;
-    error: string | null;
+	tests: Test[]
+	loading: boolean
+	error: string | null
 
-    setTests: (tests: Test[]) => void
-    addTest: (test: Test) => void
-    updateTest: (id: string, updatedTest: Partial<Test>) => void
-    deleteTest: (id: string) => void
+	setTests: (tests: Test[]) => void
+	addTest: (test: Test) => void
+	updateTest: (id: string, updatedTest: Partial<Test>) => void
+	deleteTest: (id: string) => void
 }
 
-export const TestsStore = create<ITestsStore>((set) => ({
-    tests: [],
-    loading: false,
-    error: null,
+export const TestsStore = create<ITestsStore>(set => ({
+	tests: [],
+	loading: false,
+	error: null,
 
-    setTests: (tests) => set({ tests }),
+	setTests: tests => set({ tests }),
 
-    addTest: (test) => set((state) => ({
-        tests: [...state.tests, test]
-    })),
+	addTest: test =>
+		set(state => ({
+			tests: [...state.tests, test],
+		})),
 
-    updateTest: (id, updatedTest) => set((state) => ({
-        tests: state.tests.map((test) =>
-            test.id === id ? { ...test, ...updatedTest } : test
-        )
-    })),
+	updateTest: (id, updatedTest) =>
+		set(state => ({
+			tests: state.tests.map(test =>
+				test.id === id ? { ...test, ...updatedTest } : test,
+			),
+		})),
 
-    deleteTest: (id) => set((state) => ({
-        tests: state.tests.filter((test) => test.id !== id)
-    })),
-}));
+	deleteTest: id =>
+		set(state => ({
+			tests: state.tests.filter(test => test.id !== id),
+		})),
+}))
